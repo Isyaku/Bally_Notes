17c23b7f2bae963bde49da00f398d7a814175fcf
Isyaku Mustapha <imustapha88@gmail.com>
2024-11-29 16:57:04 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Note = {
+  id: number;
+  title: string;
+  content: string;
 }
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]> ([
+    
+    {
+      id: 1,
+      title: "No title 1",
+      content: "No content 1"
+    },
+    {
+      id: 1,
+      title: "No title 1",
+      content: "No content 1"
+    },
+    {
+      id: 1,
+      title: "No title 1",
+      content: "No content 1"
+    },
+    {
+      id: 1,
+      title: "No title 1",
+      content: "No content 1"
+    }]);
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const handleAddNote = (
+      event: React.FormEvent
+    ) => { 
+      event?.preventDefault()
+      const newNote: Note = {
+        id: notes.length + 1,
+        title: title,
+        content: content
+      };
+
+      setNotes([newNote, ...notes]);
+      setTitle("");
+      setContent("");
+    };
+
+  return <div className="app-container">
+    <form
+      onSubmit={(event)=>handleAddNote(event)}
+      className="note-form"
+    ><input 
+    value={title}
+    onChange={(event)=>
+      setTitle(event?.target.value)}
+      placeholder="Title"
+      required
+    ></input>
+      <textarea
+      value={content}
+      onChange={(event)=>
+        setContent(event.target.value)
+      }
+        placeholder="Content"
+        rows={10}
+        required
+      ></textarea>
+      <button type="submit">
+        Add Note
+      </button>
+    </form>
+    <div className="notes-grid">
+      {notes.map((note)=>(
+        <div className="note-item">
+        <div className="notes-header">
+          <button>X</button>
+        </div>
+        <h2>{note.title}</h2>
+        <p>{note.content}</p>
+      </div>
+      ))}       
+    </div>
+  </div>
+};
 
 export default App;
